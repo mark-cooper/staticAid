0ea4d9e4eb6f31441b02c38544793e57c776e849
Mark Cooper <mark.c.cooper@outlook.com>
2021-10-01 09:44:13 -0700

    Add filter to get json safe values for search

---
+++ site/_plugins/json_data_filter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Jekyll
+  # Filter to ensure string input is valid as a json string value
+  module JSONDataFilter
+    def json_value(input)
+      input.encode('utf-8', 'binary', undef: :replace)
+           .to_json
+           .delete_prefix('"')
+           .delete_suffix('"')
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::JSONDataFilter)
